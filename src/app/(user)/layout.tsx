f29b248a10ea827ecd9101c91d1ad5c4255f1fb9
hvhuy279 <hvhuy279@gmail.com>
2024-04-19 14:53:08 +0700

    create app bar component

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,13 @@
+import AppHeader from "@/components/header/app.header";
+import AppFooter from "@/components/footer/app.footer";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <>
+      <AppHeader />
+      {children}
+      <AppFooter />
+    </>
+  );
+}
